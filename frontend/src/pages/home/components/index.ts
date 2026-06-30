22dc1dc03144f9f413ebe0dc5132770e56179ff8
Sam Oldenburg <sam.oldenburg@gmail.com>
2026-06-30 12:57:38 -0500

    pack lb homepage update

--- frontend/src/pages/home/components/index.ts
+++ frontend/src/pages/home/components/index.ts
@@ -1,4 +1,5 @@
 export { WelcomeBackCard } from './WelcomeBackCard';
+export { NewPackLeaderboardsCard } from './NewPackLeaderboardsCard';
 export { AboutCard } from './AboutCard';
 export { UserRecentScoresCard } from './UserRecentScoresCard';
 export { GlobalActivityCard } from './GlobalActivityCard';
